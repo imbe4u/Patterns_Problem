8541af37c97459527f7ab62bd7667e0bbea7d839
Anshu Singh <anshuonwork@gmail.com>
2024-08-11 00:15:37 +0530

    first Commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+//loop "i" is for line ->
+//loop "j" is for column |
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<" * ";
+    }
+    cout<<endl;
+}}

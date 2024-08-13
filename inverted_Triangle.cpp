72df2af072e98c019da31de0f012a44e83a66a6c
Anshu Singh <anshuonwork@gmail.com>
2024-08-13 13:38:18 +0530

    origin main

---
+++ inverted_Triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=5;k>i;k--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
